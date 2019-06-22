bbbe1f01464d86ac2a60960932aa46b7ed100a59
Aidar <brisingr@inbox.ru>
2019-06-22 20:04:03 +0600

    Initial Commit

---
+++ stepic Note task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stepic Note task
+//
+//  Created by Мак on 6/22/19.
+//  Copyright © 2019 Aidar Zhussupov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
